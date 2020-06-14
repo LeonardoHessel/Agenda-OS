b6a4d6de82023755bfef06bed9f0f4d7704c243c
Leonardo Hessel <leonardodlhessel@gmail.com>
2020-06-14 12:58:14 -0300

    Redefinição BD e Permissões
    Criado o modelo de permissão aoutomático, assim, quando um usuario for cadastrado ele recebe todas as permissões e basta alteralas quando necessário
    E quando uma novo modulo for adicionado altomáticamente todos os usuarios recebem a permissão

--- DB - DER/Agenda-OS_BD.sql
+++ DB - DER/Agenda-OS_BD.sql
@@ -1,3 +1,4 @@
+DROP DATABASE `agenda`;
 CREATE DATABASE `agenda`;
 USE `agenda`;
 
@@ -33,16 +34,90 @@ CREATE TABLE IF NOT EXISTS `permissao`(
     CONSTRAINT `fk_modulo` FOREIGN KEY (`modulo`) REFERENCES `modulo`(`id`)
 )ENGINE = InnoDB;
 
-#Criar trigger apos a insert de usuario, chamar uma procedire que pega todos os modulos insere uma permissçao para cada modulo com id do usuario e permisao = true.
+--
+-- Criação de Permissões automática --
+--
+
+DROP TRIGGER IF EXISTS `AI_Usuario_Permissoes`;
+DELIMITER $$
+CREATE TRIGGER `AI_Usuario_Permissoes` 
+AFTER INSERT ON `usuario` FOR EACH ROW
+BEGIN
+	CALL `AdcPermissoes` (new.id,'M');
+END $$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS `AI_Modulo_Permissoes`;
+DELIMITER $$
+CREATE TRIGGER `AI_Modulo_Permissoes` 
+AFTER INSERT ON `modulo` FOR EACH ROW
+BEGIN
+	CALL `AdcPermissoes` (new.id,'U');
+END $$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS `BD_Usuario_Permissoes`;
+DELIMITER $$
+CREATE TRIGGER `BD_Usuario_Permissoes` 
+BEFORE DELETE ON `usuario` FOR EACH ROW
+BEGIN
+	DELETE FROM `permissao` WHERE `usuario` = old.id;
+END $$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS `BD_Modulo_Permissoes`;
+DELIMITER $$
+CREATE TRIGGER `BD_Modulo_Permissoes` 
+BEFORE DELETE ON `modulo` FOR EACH ROW
+BEGIN
+	DELETE FROM `permissao` WHERE `modulo` = old.id;
+END $$
+DELIMITER ;
 
 
+DROP PROCEDURE IF EXISTS `AdcPermissoes`;
+DELIMITER $$
+CREATE PROCEDURE `AdcPermissoes` (UsuMod INT,Busca CHAR(1))
+BEGIN
+	DECLARE Usuario INT;
+	DECLARE Modulo INT;
+    DECLARE TotalU INT;
+    DECLARE TotalM INT;
+    DECLARE Total INT;
+    DECLARE Seq INT DEFAULT 0;    
+    SET Usuario = UsuMod;
+	SET Modulo = UsuMod;
+	SET TotalU = (SELECT COUNT(*) FROM `usuario`);
+	SET TotalM = (SELECT COUNT(*) FROM `modulo`);
+    IF Busca = 'M' THEN
+		SET Total = TotalM;
+	ELSE
+		SET Total = TotalU;
+	END IF;	
+    Loop_Busca: LOOP
+		IF Busca = 'M' THEN
+			SET Modulo = (SELECT `id` FROM `modulo` LIMIT Seq,1);
+		ELSE
+			SET Usuario = (SELECT `id` FROM `usuario` LIMIT Seq,1);
+		END IF;        
+		IF Seq < Total THEN
+			INSERT INTO `permissao` VALUES(Usuario, Modulo, TRUE);
+            SET Seq = Seq + 1;
+		ELSE
+			LEAVE Loop_Busca;
+		END IF;
+	END LOOP Loop_Busca;
+END $$
+DELIMITER ;
 
 
+DROP TABLE IF EXISTS `anotacao`;
+CREATE TABLE IF NOT EXISTS `anotacao`(
+	id int primary key auto_increment,
+    texto varchar(250)
+)ENGINE = InnoDB;
 
-
-
-
-
+-- Fim da Permissao automática
 
 
 
